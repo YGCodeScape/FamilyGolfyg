67b56eff9cf03f0ce7f25de1a973ab81a8e59de8
yashG <yrg.creation.ai@gmail.com>
2025-08-27 23:21:42 +0530

    cursor and mian gsap added

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
+function pageCursor() {
+  var div = document.querySelector("body");
+var cursor = document.querySelector(".cursor"); 
+
+div.addEventListener("mousemove", function(para){
+  const rect = cursor.getBoundingClientRect();
+  gsap.to(".cursor", {
+    x: para.x,
+    y: para.y,
+    duration: 1,
+    ease: "back.out",
+  })
+})
+}
+pageCursor();
+
+
 gsap.to(".nav", {
     backgroundColor: "rgba(0, 0, 0)",
     height: "90px",
@@ -8,5 +25,16 @@ gsap.to(".nav", {
         start: "top -5%",
         end: "top -10%",
         scrub: true
+    }
+})
+
+gsap.to(".main-div", {
+    backgroundColor: "#000",
+    scrollTrigger: {
+        trigger: ".main-div",
+        scroller: "body",
+        start: "top -25%",
+        end: "top -70%",
+        scrub: true,
     }
 })
