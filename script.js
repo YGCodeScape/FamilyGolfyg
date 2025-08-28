724c809dc5a8a470a754f9df5a35fa79d6edfc95
yashG <yrg.creation.ai@gmail.com>
2025-08-28 23:23:28 +0530

    quote section half

--- script.js
+++ script.js
@@ -77,27 +77,27 @@ gsap.from(".about-img, .about-in ", {
 })
 
 // gsap for colon img
-// gsap.from(".colon1", {
-//   y : -70,
-//   x: -70,
-//   scrollTrigger: {
-//     trigger: ".colon1",
-//     scroller: "body",
-//     start: "top 55%",
-//     end: "top 45%",
-//     scrub: 4,
-//     markers: true
-//   }
-// })
-// gsap.from(".colon2", {
-//   y : 70,
-//   x: 70,
-//   scrollTrigger: {
-//     trigger: ".colon2",
-//     scroller: "body",
-//     start: "top 55%",
-//     end: "top 45%",
-//     scrub: 4,
-//     markers: true
-//   }
-// })
+gsap.from(".quote-left", {
+  y: -70,
+  x: -70,
+  scrollTrigger: {
+    trigger: "#colon1",
+    scroller: "body",
+    markers:true,
+    start: "top 55%",
+    end: "top 45%",
+    scrub: true,
+  }
+})
+gsap.from(".quote-right", {
+  y: 70,
+  x: 70,
+  scrollTrigger: {
+    trigger: "#colon1",
+    scroller: "body",
+    // markers:true,
+    start: "top 55%",
+    end: "top 45%",
+    scrub: 2,
+  }
+})
